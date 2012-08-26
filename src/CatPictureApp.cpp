9b6c2de88eca58968af6dd2be53b1a972ed5b142
Shiloh Womack <womacksc@muohio.edu>
2012-08-25 20:40:18 -0400

    Changed background
    Slowly blinking various colors

--- src/CatPictureApp.cpp
+++ src/CatPictureApp.cpp
@@ -11,10 +11,17 @@ class CatPictureApp : public AppBasic {
 	void mouseDown( MouseEvent event );	
 	void update();
 	void draw();
+private:
+	float red;
+	float green;
+	float blue;
 };
 
 void CatPictureApp::setup()
 {
+	red = 0.0f;
+	green = 1.0f;
+	blue = 0.0f;
 }
 
 void CatPictureApp::mouseDown( MouseEvent event )
@@ -23,12 +30,27 @@ void CatPictureApp::mouseDown( MouseEvent event )
 
 void CatPictureApp::update()
 {
+	red = red + 0.001f;
+	green = red - 0.02f;
+	blue = blue + 0.005f;
+
+	if (red > 1.0f){
+		red = 0.0f;
+	}
+
+	if (green < 0.0f){
+		green = 1.0f;
+	}
+
+	if (blue > 1.0f){
+		blue = 0.0f;
+	}
 }
 
 void CatPictureApp::draw()
 {
 	// clear out the window with black
-	gl::clear( Color( 0.2f, 0.6f, 0 ) ); 
+	gl::clear( Color( red, green, blue ) ); 
 }
 
 CINDER_APP_BASIC( CatPictureApp, RendererGl )
